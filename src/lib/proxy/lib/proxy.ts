requestly/requestly-proxy

commit e5a75b0020d08ae50b81dba857ebdb30a6e63e1f

RQLY-774 fix: websocket binary frame detection (#11)

src/lib/proxy/lib/proxy.ts
@@ -1189,7 +1189,7 @@ Proxy.prototype._onWebSocketFrame = function (
   type,
   fromServer,
   data,
-  flags
+  isBinary
 ) {
   var self = this;
   async.forEach(
@@ -1221,7 +1221,7 @@ Proxy.prototype._onWebSocketFrame = function (
       if (destWebSocket.readyState === WebSocket.OPEN) {
         switch (type) {
           case "message":
-            destWebSocket.send(data, flags);
+            destWebSocket.send(data, { binary: isBinary });
             break;
           case "ping":
             destWebSocket.ping(data, flags, false);

commit 4d7ba6fe1b9d70a32f519c1345ac15bc0301c19a

fix error introduced from #11

src/lib/proxy/lib/proxy.ts
@@ -1189,7 +1189,7 @@ Proxy.prototype._onWebSocketFrame = function (
   type,
   fromServer,
   data,
-  isBinary
+  flags
 ) {
   var self = this;
   async.forEach(
@@ -1221,7 +1221,7 @@ Proxy.prototype._onWebSocketFrame = function (
       if (destWebSocket.readyState === WebSocket.OPEN) {
         switch (type) {
           case "message":
-            destWebSocket.send(data, { binary: isBinary });
+            destWebSocket.send(data, { binary: flags });
             break;
           case "ping":
             destWebSocket.ping(data, flags, false);