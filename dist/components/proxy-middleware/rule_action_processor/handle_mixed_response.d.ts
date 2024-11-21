a329acc324c6f16b097aa681c6bdd1c1a8112add
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2024-11-21 09:24:51 +0530

    feat: return content-type and content-length for local redirects (#54)

--- dist/components/proxy-middleware/rule_action_processor/handle_mixed_response.d.ts
+++ dist/components/proxy-middleware/rule_action_processor/handle_mixed_response.d.ts
@@ -10,5 +10,20 @@ declare function handleMixedResponse(ctx: any, destinationUrl: any): Promise<{
     };
 } | {
     status: boolean;
+    response_data: {
+        headers: {
+            "Content-Type": any;
+            "Content-Length": number;
+            "Cache-Control": string;
+        } | {
+            "Cache-Control": string;
+            "Content-Type"?: undefined;
+            "Content-Length"?: undefined;
+        };
+        status_code: number;
+        body: string;
+    };
+} | {
+    status: boolean;
     response_data?: undefined;
 }>;
--- dist/components/proxy-middleware/rule_action_processor/handle_mixed_response.js
+++ dist/components/proxy-middleware/rule_action_processor/handle_mixed_response.js
@@ -30,6 +30,7 @@ const axios = require("axios");
 const parser = require("ua-parser-js");
 const fs_1 = __importDefault(require("fs"));
 const Sentry = __importStar(require("@sentry/browser"));
+const mime = require('mime-types');
 const handleMixedResponse = async (ctx, destinationUrl) => {
     var _a, _b, _c;
     // Handling mixed response from safari
@@ -71,14 +72,22 @@ const handleMixedResponse = async (ctx, destinationUrl) => {
     if (destinationUrl === null || destinationUrl === void 0 ? void 0 : destinationUrl.startsWith("file://")) {
         const path = destinationUrl.slice(7);
         try {
-            // utf-8 is common assumption, but this introduces edge cases
-            const data = fs_1.default.readFileSync(path, "utf-8");
+            const buffers = fs_1.default.readFileSync(path);
+            const mimeType = mime.lookup(path);
+            const bodyContent = buffers.toString("utf-8"); // assuming utf-8 encoding
+            const headers = mimeType ? {
+                "Content-Type": mimeType,
+                "Content-Length": Buffer.byteLength(bodyContent),
+                "Cache-Control": "no-cache"
+            } : {
+                "Cache-Control": "no-cache"
+            };
             return {
                 status: true,
                 response_data: {
-                    headers: { "Cache-Control": "no-cache" },
+                    headers,
                     status_code: 200,
-                    body: data,
+                    body: buffers.toString("utf-8"), // assuming utf-8 encoding
                 },
             };
         }
