d7891c50d309738a4789158b1998bf41772bc712
Sahil Gupta <wrongsahil@gmail.com>
2025-05-13 21:38:32 +0530

    fix: request with brotli content encoding

--- src/lib/proxy/index.js
+++ src/lib/proxy/index.js
@@ -1,5 +1,5 @@
-import DefaultExport, {Proxy, PROXY_HANDLER_TYPE, gunzip, wildcard} from "./lib/proxy"
+import DefaultExport, {Proxy, PROXY_HANDLER_TYPE, gunzip, decompress, wildcard} from "./lib/proxy"
 
 export default DefaultExport;
 
-export {Proxy, PROXY_HANDLER_TYPE, gunzip, wildcard}
+export {Proxy, PROXY_HANDLER_TYPE, gunzip, decompress, wildcard}
