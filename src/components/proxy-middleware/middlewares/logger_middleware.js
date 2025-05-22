88db00003d10375c20700c37b347c1683e5701a2
nsrCodes <navdeep@requestly.io>
2025-05-22 19:35:06 +0530

    draft: try-catch around logger middleware

--- src/components/proxy-middleware/middlewares/logger_middleware.js
+++ src/components/proxy-middleware/middlewares/logger_middleware.js
@@ -27,47 +27,11 @@ class LoggerMiddleware {
   };
 
   send_network_log = (ctx, action_result_objs = [],requestState="") => {
-    // let query_params_string;
-    // if (ctx.rq.final_request.query_params) {
-    //   query_params_string = JSON.stringify(ctx.rq.final_request.query_params);
-    // }
-    // const log = {
-    //   id: ctx.uuid,
-    //   timestamp: Math.floor(Date.now() / 1000),
-    //   url: url.parse(
-    //     (ctx.isSSL ? "https://" : "http://") +
-    //       ctx.clientToProxyRequest.headers.host +
-    //       ctx.clientToProxyRequest.url
-    //   ).href,
-    //   request: {
-    //     method: ctx.rq.final_request.method,
-    //     path: ctx.rq.final_request.host,
-    //     host: ctx.rq.final_request.host,
-    //     port: ctx.rq.final_request.port,
-    //     headers: ctx.rq.final_request.headers,
-    //     body: ctx.rq.final_request.body,
-    //     query_params: query_params_string,
-    //   },
-    //   requestShellCurl: this.generate_curl_from_har(
-    //     ctx.rq.final_request.requestHarObject
-    //   ),
-    //   response: {
-    //     statusCode: ctx.rq.final_response.status_code,
-    //     headers: ctx.rq.final_response.headers || {},
-    //     contentType:
-    //       (ctx.rq.final_response.headers &&
-    //         ctx.rq.final_response.headers["content-type"] &&
-    //         ctx.rq.final_response.headers["content-type"].includes(";") &&
-    //         ctx.rq.final_response.headers["content-type"].split(";")[0]) ||
-    //       (ctx.rq.final_response.headers &&
-    //         ctx.rq.final_response.headers["content-type"]),
-    //     body: ctx.rq.final_response.body || null,
-    //   },
-    //   actions: get_success_actions_from_action_results(action_result_objs),
-    // };
-    // ipcRenderer.send("log-network-request", log);
-    // TODO: Sending log for now. Ideally this should be har object
-    this.loggerService.addLog(this.createLog(ctx, action_result_objs, requestState), ctx.rq.final_request.headers || {})
+    try {
+      this.loggerService.addLog(this.createLog(ctx, action_result_objs, requestState), ctx.rq.final_request.headers || {})
+    } catch (error) {
+      console.log("Error in LoggerMiddleware.send_network_log: ", error);
+    }
   };
 
   createLog = (ctx, action_result_objs = [], requestState="") => {
